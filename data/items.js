f2a93d763c60866871563532a76c4deffeb285a4
Andrew Bradt <andrewjbradt@gmail.com>
2022-05-16 16:19:55 -0400

    add mock item data

---
+++ data/items.js
@@ -0,0 +1,14 @@
+const items = [
+  {
+    id: 1,
+    name: 'Nice Jewellery',
+    description: 'This is a nice ring here',
+    imgURL: 'https://image.brilliantearth.com/media/diamond_ring_vto/4S/BE1D6013_yellow_Oval_top_75_carat.png'
+  },
+  {
+    id: 2,
+    name: 'Other Jewellery',
+    description: 'This is another ring',
+    imgURL: 'https://www.brides.com/thmb/dxoeOO4Wt4RgJP1DQm56XBGYyAs=/500x350/filters:no_upscale():max_bytes(200000):strip_icc()/GettyImages-75651642-ea5de688092c47338639a7b5718164e1.jpg'
+  }
+];
