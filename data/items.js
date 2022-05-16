0095b8108b8b23cdeb8f8bda1cc90886204bdf9d
Andrew Bradt <andrewjbradt@gmail.com>
2022-05-16 17:11:07 -0400

    render index with items list, add method-override package

--- data/items.js
+++ data/items.js
@@ -3,12 +3,14 @@ const items = [
     id: 1,
     name: 'Nice Jewellery',
     description: 'This is a nice ring here',
-    imgURL: 'https://image.brilliantearth.com/media/diamond_ring_vto/4S/BE1D6013_yellow_Oval_top_75_carat.png'
+    imgURL: 'https://i.pcmag.com/imagery/reviews/04LOytI6kS2pMMEsZr1vsUq-1..v1607958887.jpg'
   },
   {
     id: 2,
     name: 'Other Jewellery',
     description: 'This is another ring',
-    imgURL: 'https://www.brides.com/thmb/dxoeOO4Wt4RgJP1DQm56XBGYyAs=/500x350/filters:no_upscale():max_bytes(200000):strip_icc()/GettyImages-75651642-ea5de688092c47338639a7b5718164e1.jpg'
+    imgURL: 'https://i.pcmag.com/imagery/reviews/04LOytI6kS2pMMEsZr1vsUq-1..v1607958887.jpg'
   }
 ];
+
+module.exports = items;
