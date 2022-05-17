6efe144adfc6fde38926f8c9759ec7d477ace9fb
Andrew Bradt <andrewjbradt@gmail.com>
2022-05-17 14:19:24 -0400

    add index front-end script

---
+++ public/javascripts/index.js
@@ -0,0 +1,11 @@
+(() => {
+  const button = document.getElementById('toggle-add-item');
+  const form = document.getElementById('add-item');
+  let hiddenState = true;
+
+  button.addEventListener('click', () => {
+    hiddenState = !hiddenState;
+    button.innerText = (hiddenState) ? 'Add Item' : 'Cancel';
+    form.hidden = hiddenState;
+  });
+})();
