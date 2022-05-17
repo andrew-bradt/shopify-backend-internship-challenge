279b27bb5f58ca2fc6e89150eeb7fc50f044baad
Andrew Bradt <andrewjbradt@gmail.com>
2022-05-17 17:13:24 -0400

    add NODE_ENV to start and dev scripts

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const indexRouter = require('./routes/index');
 const itemRouter = require('./routes/item');
 
 const app = express();
-
+console.log(process.env);
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -17,7 +17,6 @@ app.set('view engine', 'ejs');
 app.use(methodOverride('_method'));
 app.use(logger('dev'));
 app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
