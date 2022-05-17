fd9664f75ba8e9feb60a9ec404bfc276162ddfd9
Andrew Bradt <andrewjbradt@gmail.com>
2022-05-17 19:11:34 -0400

    add item, inventory and delete caches, and closure for tracking if inventory cache is current

--- helpers/cache.js
+++ helpers/cache.js
@@ -1,2 +1,25 @@
 const {Cache} = require('memory-cache');
 
+const itemCache = new Cache();
+const inventoryCache = new Cache();
+const deleteCache = new Cache();
+
+const trackInventoryCache = () => {
+  let state = false;
+  const isInventoryCurrent = () => state;
+  const setInventoryToStale = () => state = false;
+  const setInventoryToCurrent = () => state = true;
+  
+  return {isInventoryCurrent, setInventoryToCurrent, setInventoryToStale};
+};
+
+const {isInventoryCurrent, setInventoryToCurrent, setInventoryToStale} = trackInventoryCache();
+
+module.exports = {
+  itemCache,
+  inventoryCache,
+  deleteCache,
+  isInventoryCurrent,
+  setInventoryToCurrent,
+  setInventoryToStale
+};
