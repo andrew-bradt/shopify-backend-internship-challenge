5d27b2d11fa6ebb2c01c657d13aa842b569c2313
Andrew Bradt <andrewjbradt@gmail.com>
2022-05-18 16:26:18 -0400

    add comments to cache.js

--- helpers/cache.js
+++ helpers/cache.js
@@ -1,11 +1,17 @@
 const {Cache} = require('memory-cache');
 
 const ONE_DAY_MS = 24 * 60 * 60 * 1000;
+
 const itemCache = new Cache();
 const inventoryCache = new Cache();
 const deleteCache = new Cache();
 const commentCache = new Cache();
 
+/* 
+Helper function for determining whether the inventoryCache is up-to-date or stale
+Invoke setInventoryToStale when an item is added, modified or deleted
+Invoke setInventoryToCurrent after a query has been made when retrieving entire inventory
+*/
 const trackInventoryCache = () => {
   let state = false;
   const isInventoryCurrent = () => state;
